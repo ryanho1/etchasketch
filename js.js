77b86a4a4bd9ce25b13d3c5cca1e37db5f63f6e4
ryanho1 <ryan81645@gmail.com>
2024-09-06 14:59:20 +0800

    grid created 16x16

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
-console.log('con');
+const container = document.querySelector("#container");
+// container.style.backgroundColor = "grey";
+container.style.borderColor = "black";
+
+function Grid() {
+    for (let i = 0; i<256; i++){
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        container.appendChild(grid);
+        grid.textContent = "i";
+    }
+}
+
+Grid();
