38de7c8787a62ccfcbc48d715550f88a49b63524
ryanho1 <ryan81645@gmail.com>
2024-09-10 01:20:05 +0800

    handled deletion and creation of new grid

--- js.js
+++ js.js
@@ -1,29 +1,45 @@
+// query selectors (createdDivs is to delete the grid)
 const container = document.querySelector("#container");
 const userInput = document.querySelector("#userInput");
-
-let input = 0;
-
-function Grid(input,input) {
-
-
-
-    for (let i = 0; i<(input*input); i++){
-    userInput.addEventListener("click", () =>  {
-        container.removeChild(grid);
-        input = prompt("What grid would you like?");
-    });
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        container.appendChild(grid);
-        grid.addEventListener("mouseover", (event) => {
-            grid.setAttribute("style", "background: black;");
-        });
-        // grid.addEventListener("mouseout", (event) => {
-        //     grid.setAttribute("style", "background: ");
-        //     });
-    }
-    //
-}
+const createdDivs = document.getElementById("grid");
+const gridSize = 662;
+let limit = 16;
 
 Grid(16,16);
+
+    userInput.addEventListener("click", () =>  {
+        resetGrid();
+        let input = parseInt(prompt("What size of a grid would you like?"));
+        if (input < 100){
+            Grid(input,input);
+        } else {while (input > 100) {
+            input = prompt("Less than 100 please");
+            if (input < 100) {
+                Grid(input,input);
+            }
+        }
+            } 
+    });
+
+function Grid(input,input) {
+    for (let i = 0; i<(input*input); i++){
+        let cellsize = gridSize / limit;
+        const grid = document.createElement("div");
+        container.appendChild(grid);
+        grid.classList.add("grid");
+        grid.style.width = `${cellsize}px`;
+        grid.style.height = `${cellsize}px`;
+        grid.addEventListener("mouseover", (event) => {
+        grid.setAttribute("style", "background: black;");
+        });
+    }
+}
+
+function resetGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+}
+
+
 
